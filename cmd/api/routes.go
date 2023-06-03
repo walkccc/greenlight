4a692f7995f8ec3aeb6a54a816d98198f223a925
Peng-Yu Chen <me@pengyuc.com>
2023-06-03 10:26:42 -0400

    13.3 Registering a User

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,6 +20,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.createUserHandler)
+
 	standard := alice.New(app.recoverPanic, app.rateLimit)
 	return standard.Then(router)
 }
