e9c1e19d46b2e6704f474d421a846487384c9f02
Peng-Yu Chen <me@pengyuc.com>
2023-06-03 22:26:59 -0400

    15.4 Activating a User

--- internal/data/users.go
+++ internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"time"
@@ -92,6 +93,7 @@ func ValidateUser(v *validator.Validator, user *User) {
 type UserModelInterface interface {
 	Create(user *User) error
 	GetByEmail(email string) (*User, error)
+	GetForToken(scope, tokenPlaintext string) (*User, error)
 	Update(user *User) error
 }
 
@@ -150,6 +152,54 @@ func (m UserModel) GetByEmail(email string) (*User, error) {
 	defer cancel()
 
 	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+
+	query := `
+		SELECT users.id,
+			users.created_at,
+			users.name,
+			users.email,
+			users.password_hash,
+			users.activated,
+			users.version
+		FROM users
+			INNER JOIN tokens ON users.id = tokens.user_id
+		WHERE tokens.hash = $1
+			AND tokens.scope = $2
+			AND tokens.expiry > $3
+	`
+	args := []any{
+		tokenHash[:],
+		tokenScope,
+		time.Now(),
+	}
+
+	var user User
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
 		&user.ID,
 		&user.CreatedAt,
 		&user.Name,
