4a82d7aa45e056f6d4e64552bc0697168dcaf015
Peng-Yu Chen <me@pengyuc.com>
2023-06-25 15:05:38 -0400

    9.6 Sorting Lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -109,15 +110,15 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 		SELECT
 			id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE
 			(to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 			AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id
-	`
+		ORDER BY %s %s, id ASC
+	`, filters.sortColumn(), filters.sortDirection())
 	args := []any{
 		title,
 		pq.Array(genres),
