aa25f5c707dac11efca402761107b22ff08de6e1
Peng-Yu Chen <me@pengyuc.com>
2023-06-25 15:06:53 -0400

    9.5 Full-Text Search

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -114,7 +114,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 			id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE
-			(LOWER(title) = LOWER($1) OR $1 = '')
+			(to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 			AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id
 	`
