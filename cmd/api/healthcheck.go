453c4bd1e1ec0f79049c647f26b110221daa9747
Peng-Yu Chen <me@pengyuc.com>
2023-12-05 23:25:56 -0500

    3.1 Fixed-Format JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,15 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err := w.Write([]byte(js))
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem to write the response.", http.StatusInternalServerError)
+		return
+	}
 }
