aa7079703c39f8a951d6d9edc6e8abc399b22bd5
Peng-Yu Chen <me@pengyuc.com>
2023-12-06 09:32:30 -0500

    3.3 Encoding Structs
    * `-` can be used when you never want a particular struct field to appear in the JSON output
    * `omitempty` directive hides a field in the JSON output if and only if the struct field value is empty

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/walkccc/greenlight/internal/data"
 )
 
 // createMovieHandler handles requests for "POST /v1/movies".
@@ -18,5 +21,22 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	fmt.Fprintf(w, "get the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(
+			w,
+			"The server encountered a problem and could not process your request",
+			http.StatusInternalServerError,
+		)
+	}
 }
