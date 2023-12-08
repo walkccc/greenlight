f1da24cd53a7975e818ac75d3c5a864289d01bc9
Peng-Yu Chen <me@pengyuc.com>
2023-12-07 21:03:26 -0500

    4.4 Custom JSON Decoding
    ```
    $ curl -d '{"title": "Moana", "runtime": "107 mins"}' localhost:4000/v1/movies
    {Title:Moana Year:0 Runtime:107 Genres:[]}
    ```

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,10 +15,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// are a subset of the Movie struct that we created earlier). This struct will
 	// be our *target decode destination*.
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+// ErrInvalidRuntimeFormat is an error that UnmarshalJSON() can return if we're
+// unable to parse or convert the JSON string.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -14,4 +20,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON ensures that Runtime satisfies the json.Unmarshaler interface.
+// IMPORTANT: because UnmarshalJSON() needs to modify the receiver (our Runtime
+// type), we must use a pointer receiver for this to work correctly. Otherwise,
+// we will only be modifying a copy (which is then discarded when this method
+// returns).
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	num, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(num)
+	return nil
 }
