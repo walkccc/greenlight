a3e647030a378f80db9a0757ca1fe5a11a7bb3e5
Peng-Yu Chen <me@pengyuc.com>
2023-12-08 11:36:13 -0500

    7.3 Fetching a Movie
    ```
    $ curl -i localhost:4000/v1/movies/2
    HTTP/1.1 200 OK
    Content-Type: application/json
    Date: Tue, 12 Dec 2023 23:16:16 GMT
    Content-Length: 161
    
    {
      "movie": {
        "id": 2,
        "title": "Black Panther",
        "year": 2018,
        "runtime": "134 mins",
        "genres": [
          "action",
          "adventure"
        ],
        "version": 1
      }
    }
    ```
    
    ```
    $ curl -i localhost:4000/v1/movies/42
    HTTP/1.1 404 Not Found
    Content-Type: application/json
    Date: Tue, 12 Dec 2023 23:16:22 GMT
    Content-Length: 59
    
    {
      "error": "The requested resource could not be found."
    }
    ```

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/walkccc/greenlight/internal/data"
 	"github.com/walkccc/greenlight/internal/validator"
@@ -65,13 +65,15 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
