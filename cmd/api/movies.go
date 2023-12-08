3adb8dd2ec428a0acbf0a1ed58c0c3a1d7b9cc7c
Peng-Yu Chen <me@pengyuc.com>
2023-12-08 16:23:22 -0500

    9.2 Validating Query String Parameters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -26,7 +26,11 @@ func (app *application) getMoviesHandler(w http.ResponseWriter, r *http.Request)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 	input.Filters.Sort = app.readString(qs, "sort", "id")
 
-	if !v.Valid() {
+	input.Filters.SortSafeValues = []string{
+		"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime",
+	}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,18 @@
 package data
 
+import "github.com/walkccc/greenlight/internal/data/validator"
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page           int
+	PageSize       int
+	Sort           string
+	SortSafeValues []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafeValues...), "sort", "invalid sort value")
 }
