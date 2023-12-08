dd317de77091399b4db5ebe8f9fce662a680b877
Peng-Yu Chen <me@pengyuc.com>
2023-12-08 11:12:01 -0500

    7.2 Creating a New Movie
    Create four records to demonstrate different functionality in the
    following chapters.
    
    ```
    $ BODY='{"title":"Moana","year":2016,"runtime":"107 mins", "genres":["animation","adventure"]}'
    $ curl -i -d "$BODY" localhost:4000/v1/movies
    HTTP/1.1 201 Created
    Content-Type: application/json
    Location: /v1/movies/1
    Date: Tue, 12 Dec 2023 22:54:46 GMT
    Content-Length: 156
    
    {
      "movie": {
        "id": 1,
        "title": "Moana",
        "year": 2016,
        "runtime": "107 mins",
        "genres": [
          "animation",
          "adventure"
        ],
        "version": 1
      }
    }
    ```
    
    ```
    $ BODY='{"title":"Black Panther","year":2018,"runtime":"134 mins","genres":["action","adventure"]}'
    $ curl -d "$BODY" localhost:4000/v1/movies
    {
      "movie": {
        "id": 2,
        "title": "Black Panther",
        "year": 2018,
        "runtime": "134 mins",
        "genres": [
          "action",
          "adventure"
        ],
        "version": 1
      }
    }
    ```
    
    ```
    $ BODY='{"title":"Deadpool","year":2016, "runtime":"108 mins","genres":["action","comedy"]}'
    $ curl -d "$BODY" localhost:4000/v1/movies
    {
      "movie": {
        "id": 3,
        "title": "Deadpool",
        "year": 2016,
        "runtime": "108 mins",
        "genres": [
          "action",
          "comedy"
        ],
        "version": 1
      }
    }
    ```
    
    ```
    $ BODY='{"title":"The Breakfast Club","year":1986, "runtime":"96 mins","genres":["drama"]}'
    $ curl -d "$BODY" localhost:4000/v1/movies
    {
      "movie": {
        "id": 4,
        "title": "The Breakfast Club",
        "year": 1968,
        "runtime": "96 mins",
        "genres": [
          "drama"
        ],
        "version": 1
      }
    }
    ```
    
    ```
    greenlight=> SELECT * FROM "Movies";
     id |       created_at       |       title        | year | runtime |        genres         | version
    ----+------------------------+--------------------+------+---------+-----------------------+---------
      1 | 2023-12-12 22:54:47+00 | Moana              | 2016 |     107 | {animation,adventure} |       1
      2 | 2023-12-12 22:55:23+00 | Black Panther      | 2018 |     134 | {action,adventure}    |       1
      3 | 2023-12-12 22:55:37+00 | Deadpool           | 2016 |     108 | {action,comedy}       |       1
      4 | 2023-12-12 22:55:55+00 | The Breakfast Club | 1986 |      96 | {drama}               |       1
    (4 rows)
    
    greenlight=>
    ```

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -42,7 +42,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Create(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // getMovieHandler handles requests for "GET /v1/movies/:id".
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/walkccc/greenlight/internal/validator"
 )
 
@@ -46,7 +47,17 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Create(movie *Movie) error {
-	return nil
+	query := `
+		INSERT INTO "Movies" (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+	}
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
