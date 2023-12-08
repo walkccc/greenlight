da3f9c9acdc7f31868f263be49f3a5663bd35573
Peng-Yu Chen <me@pengyuc.com>
2023-12-08 11:56:37 -0500

    7.5 Deleting a Movie
    ```
    $ curl -X DELETE localhost:4000/v1/movies/3
    {
      "message": "movie successfully deleted"
    }
    ```
    
    ```
    $ curl -X DELETE localhost:4000/v1/movies/3
    {
      "error": "The requested resource could not be found."
    }
    ```
    
    ```
    greenlight=> SELECT * FROM "Movies";
     id |       created_at       |       title        | year | runtime |          genres           | version
    ----+------------------------+--------------------+------+---------+---------------------------+---------
      1 | 2023-12-12 22:54:47+00 | Moana              | 2016 |     107 | {animation,adventure}     |       1
      4 | 2023-12-12 22:55:55+00 | The Breakfast Club | 1986 |      96 | {drama}                   |       1
      2 | 2023-12-12 22:55:23+00 | Black Panther      | 2018 |     134 | {sci-fi,action,adventure} |       2
    (3 rows)
    
    greenlight=>
    ```

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -137,3 +137,28 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+// deleteMovieHandler handles requests for "DELETE /v1/movies/:id".
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getMovieHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
 	standard := alice.New(app.recoverPanic)
 	return standard.Then(router)
