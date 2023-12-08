2b579894c49ca422f78917df484a76fa1790125f
Peng-Yu Chen <me@pengyuc.com>
2023-12-08 11:53:46 -0500

    7.4 Updating a Movie
    The current record:
    
    ```
    $ curl localhost:4000/v1/movies/2
    {
      "movie": {
        "id": 2,
        "title": "Black Panther",
        "year": 2018,
        "runtime": "134 mins",
        "genres": [
          "action",
          "adventure"
        ],
        "version": 1
      }
    }
    ```
    
    The updated record:
    
    ```
    $ BODY='{"title":"Black Panther","year":2018,"runtime":"134 mins","genres":["sci-fi","action","adventure"]}'
    $ curl -X PUT -d "$BODY" localhost:4000/v1/movies/2
    {
      "movie": {
        "id": 2,
        "title": "Black Panther",
        "year": 2018,
        "runtime": "134 mins",
        "genres": [
          "sci-fi",
          "action",
          "adventure"
        ],
        "version": 2
      }
    }
    ```

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -95,7 +95,19 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		UPDATE "Movies"
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
