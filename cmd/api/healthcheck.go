c570c597f93b5044d0f5a02126189735b331f4b9
Peng-Yu Chen <me@pengyuc.com>
2023-12-09 09:38:13 -0500

    undo demo: 11.3 Executing the Shutdown

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +12,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 			"version":     version,
 		},
 	}
-
-	time.Sleep(4 * time.Second)
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
