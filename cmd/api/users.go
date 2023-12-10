d658347d5ca1a4395acbc726f8b761495e3e8035
Peng-Yu Chen <me@pengyuc.com>
2023-12-10 17:53:34 -0500

    14.3 Sending Activation Tokens
    ```
    $ BODY='{"name": "Faith Smith", "email": "faith@example.com", "password": "pa55word"}'
    $ curl -d "$BODY" localhost:4000/v1/users
    {
      "user": {
        "id": 6,
        "created_at": "2023-12-14T03:53:04Z",
        "name": "Faith Smith",
        "email": "faith@example.com",
        "activated": false
      }
    }
    ```
    
    ```
    greenlight=> SELECT * FROM "Tokens";
                                    hash                                | user_id |            expiry             |   scope
    --------------------------------------------------------------------+---------+-------------------------------+------------
     \xb26a1005c106ff186dc1ccc2d30bb00e1fabb581a3226d55e845681c47c2544c |       6 | 2023-12-17 03:53:04.106515+00 | activation
    (1 row)
    
    greenlight=>
    ```

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/walkccc/greenlight/internal/data"
 	"github.com/walkccc/greenlight/internal/validator"
@@ -52,8 +53,18 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.serverErrorResponse(w, r, err)
 			return
