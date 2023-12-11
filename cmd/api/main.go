16b6e7ee4157081877779fdfdf5df21722098ebc
Peng-Yu Chen <me@pengyuc.com>
2023-12-11 00:11:49 -0500

    19.6 Managing and Automating Version Numbers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -15,9 +16,12 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/walkccc/greenlight/internal/data"
 	"github.com/walkccc/greenlight/internal/mailer"
+	"github.com/walkccc/greenlight/internal/vcs"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 // config holds all the configuration settings for our application.
 type config struct {
@@ -95,7 +99,14 @@ func main() {
 		},
 	)
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
